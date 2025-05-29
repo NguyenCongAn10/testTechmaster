f88d669326b95030c7e0ae75de0a11c8175bb0b3
NguyenCongAn <nan369641@gmail.com>
2025-05-29 23:06:28 +0700

    ad 05

--- 05/flutter_project_05/lib/main.dart
+++ 05/flutter_project_05/lib/main.dart
@@ -1,122 +1,123 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(TriangleApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+class TriangleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Kéo Từng Đỉnh Tam Giác')),
+        body: TriangleEditor(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class TriangleEditor extends StatefulWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _TriangleEditorState createState() => _TriangleEditorState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _TriangleEditorState extends State<TriangleEditor> {
+  List<Offset> points = [
+    Offset(100, 100), // A
+    Offset(200, 100), // B
+    Offset(150, 200), // C
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int? draggingIndex;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    final size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      onPanStart: (details) {
+        print('Pan Start: ${details.localPosition}');
+        setState(() {
+          for (int i = 0; i < points.length; i++) {
+            final distance = (details.localPosition - points[i]).distance;
+            print('Distance to point $i: $distance');
+            if (distance < 30) {
+              draggingIndex = i;
+              print('Selected point: $i');
+              break;
+            }
+          }
+        });
+      },
+      onPanUpdate: (details) {
+        if (draggingIndex != null) {
+          setState(() {
+            // Tạo một bản sao mới của danh sách points
+            final newPoints = List<Offset>.from(points);
+            // Cập nhật tọa độ mới với Offset mới
+            newPoints[draggingIndex!] = Offset(
+              details.localPosition.dx.clamp(0, size.width),
+              details.localPosition.dy.clamp(0, size.height),
+            );
+            points = newPoints;
+            print('Updated points: $points');
+          });
+        }
+      },
+      onPanEnd: (_) {
+        setState(() {
+          draggingIndex = null;
+          print('Pan End');
+        });
+      },
+      child: Stack(
+        children: [
+          CustomPaint(painter: TrianglePainter(points), size: Size.infinite),
+          Container(
+            color: Colors.transparent,
+            width: size.width,
+            height: size.height,
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class TrianglePainter extends CustomPainter {
+  final List<Offset> points;
+
+  TrianglePainter(this.points);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint =
+        Paint()
+          ..color = Colors.blue
+          ..strokeWidth = 3
+          ..style = PaintingStyle.stroke;
+
+    final path =
+        Path()
+          ..moveTo(points[0].dx, points[0].dy)
+          ..lineTo(points[1].dx, points[1].dy)
+          ..lineTo(points[2].dx, points[2].dy)
+          ..close();
+
+    canvas.drawPath(path, paint);
+
+    final dotPaint = Paint()..color = Colors.red;
+    for (final p in points) {
+      canvas.drawCircle(p, 8, dotPaint);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant TrianglePainter oldDelegate) {
+    // So sánh từng tọa độ để đảm bảo vẽ lại khi có thay đổi
+    for (int i = 0; i < points.length; i++) {
+      if (points[i] != oldDelegate.points[i]) {
+        return true;
+      }
+    }
+    return false;
   }
 }
